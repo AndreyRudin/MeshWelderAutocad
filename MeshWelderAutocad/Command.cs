f21c52c6be0ad5f41d24caba3cf9e9a28c6a151b
AndreyRudin <rudin1996@mail.ru>
2023-10-11 01:27:39 +0400

    изм

--- MeshWelderAutocad/Command.cs
+++ MeshWelderAutocad/Command.cs
@@ -18,6 +18,7 @@ using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Shapes;
 using static Autodesk.AutoCAD.LayerManager.LayerFilter;
+using static System.Net.Mime.MediaTypeNames;
 using acadApp = Autodesk.AutoCAD.ApplicationServices.Application;
 using Line = Autodesk.AutoCAD.DatabaseServices.Line;
 using Path = System.IO.Path;
@@ -27,6 +28,10 @@ namespace MeshWelderAutocad
     public class Mesh
     {
         public string DwgName { get; set; }
+        public string View { get; set; }
+        public string Rigth { get; set; }
+        public string Normal { get; set; }
+        public string Up { get; set; }
         public List<MyRebar> Rebars { get; set; } = new List<MyRebar>();
     }
     public class MyRebar
@@ -43,7 +48,7 @@ namespace MeshWelderAutocad
         public void TestCom()
         {
             //Панель создать Ribbon
-            //внедрить отправку данных о запуск
+            //внедрить отправку данных о запуск - файл отправлять на почту например или просто на какой-то хостинг, где я буду в БД его записывать, время запуска, имя модели, размер модели
             //+?проверить что нет дублирования в каких панелях?
             //Написать проверку, возможно ли вообще десериализровать json таким образом, если нет, то сообщение и остановка плагина
             //Вызов команды доступен даже если нету открытого чертежа
@@ -73,7 +78,7 @@ namespace MeshWelderAutocad
             }
             catch
             {
-                MessageBox.Show("Некорректный JSON. Обратитесь к разработчику");
+                MessageBox.Show("Некорректный JSON. Требуется выбрать корректный файл");
                 return;
             }
 
@@ -94,13 +99,54 @@ namespace MeshWelderAutocad
                     {
                         BlockTable blockTable = tr.GetObject(db.BlockTableId, OpenMode.ForRead) as BlockTable;
                         BlockTableRecord modelSpace = tr.GetObject(blockTable[BlockTableRecord.ModelSpace], OpenMode.ForWrite) as BlockTableRecord;
-                        LayerTable layerTable = (LayerTable)tr.GetObject(db.LayerTableId, OpenMode.ForRead);
+                        LayerTable layerTable = (LayerTable)tr.GetObject(db.LayerTableId, OpenMode.ForWrite);
 
-                        LayerTableRecord layerNull = (LayerTableRecord)tr.GetObject(layerTable["0"], OpenMode.ForWrite);
-                        layerNull.IsOff = true;
-                        layerNull.IsLocked = true;
-                        layerNull.IsFrozen = true;
+                        //int heigth = -200;
+                        //int x = -200;
+                        //var text = new MText();
+                        //text.Contents = "View: " + mesh.View;
+                        //text.Location = new Point3d(x, heigth, 0);
+                        //text.LayerId = layerTable["MESH"];
+                        //text.TextHeight = 150;
+                        //modelSpace.AppendEntity(text);
+                        //tr.AddNewlyCreatedDBObject(text, true);
+                        //heigth -= 200;
 
+                        //var text2 = new MText();
+                        //text2.Contents = "Rigth: " + mesh.Rigth;
+                        //text2.Location = new Point3d(x, heigth, 0);
+                        //text2.LayerId = layerTable["MESH"];
+                        //text2.TextHeight = 150;
+                        //modelSpace.AppendEntity(text2);
+                        //tr.AddNewlyCreatedDBObject(text2, true);
+                        //heigth -= 200;
+
+                        //var text3 = new MText();
+                        //text3.Contents = "Up: " + mesh.Up;
+                        //text3.Location = new Point3d(x, heigth, 0);
+                        //text3.LayerId = layerTable["MESH"];
+                        //text3.TextHeight = 150;
+                        //modelSpace.AppendEntity(text3);
+                        //tr.AddNewlyCreatedDBObject(text3, true);
+                        //heigth -= 200;
+
+                        //var text5 = new MText();
+                        //text5.Contents = "Normal: " + mesh.Normal;
+                        //text5.Location = new Point3d(x, heigth, 0);
+                        //text5.LayerId = layerTable["MESH"];
+                        //text5.TextHeight = 150;
+                        //modelSpace.AppendEntity(text5);
+                        //tr.AddNewlyCreatedDBObject(text5, true);
+                        //heigth -= 200;
+
+                        //var text6 = new MText();
+                        //text6.Contents = "Name view: " + mesh.DwgName;
+                        //text6.Location = new Point3d(x, heigth, 0);
+                        //text6.LayerId = layerTable["MESH"];
+                        //text6.TextHeight = 150;
+                        //modelSpace.AppendEntity(text6);
+                        //tr.AddNewlyCreatedDBObject(text6, true);
+                        //heigth -= 200;
 
                         foreach (var rebar in mesh.Rebars)
                         {
@@ -108,10 +154,14 @@ namespace MeshWelderAutocad
                             line.Color = GetColor(rebar.Diameter);
                             ObjectId layerId = layerTable["MESH"];
                             line.LayerId = layerId;
-
                             modelSpace.AppendEntity(line);
                             tr.AddNewlyCreatedDBObject(line, true);
                         }
+
+                        LayerTableRecord layerNull = (LayerTableRecord)tr.GetObject(layerTable["0"], OpenMode.ForWrite);
+                        layerNull.IsOff = true;
+                        layerNull.IsLocked = true;
+                        layerNull.IsFrozen = true;
                         tr.Commit();
                     }
                     newDoc.Database.SaveAs(path, true, DwgVersion.Current, null);
