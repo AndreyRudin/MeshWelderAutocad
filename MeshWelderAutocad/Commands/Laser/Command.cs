9e2fede55a9db07bf76bbba730455af2b3b35d96
AndreyRudin <rudin1996@mail.ru>
2024-11-14 10:57:16 +0300

    лазер

--- MeshWelderAutocad/Commands/Laser/Command.cs
+++ MeshWelderAutocad/Commands/Laser/Command.cs
@@ -14,13 +14,16 @@ using acadApp = Autodesk.AutoCAD.ApplicationServices.Application;
 
 namespace MeshWelderAutocad.Commands.Laser
 {
-    //TODO зеркальность проверить!
+    //TODO Где полилинии, а где отрезками делаем?
+    //TODO габариты панели и проемов и расстояния, положения деталей с ревитом на всех 4 поворотах проверить
+    //TODO найти панель где все типы закладух будует 5,6,7,9 и проверить как отрисовывается
     internal class Command
     {
         private static Dtos.Panel _panel;
         private static Transaction _activeTransaction;
         private static LayerTable _layerTable;
         private static BlockTableRecord _modelSpace;
+        private static Database _db;
         [CommandMethod("CreateDrawingsForLaser")]
         public static void CreateDrawingsForLaser()
         {
@@ -37,28 +40,28 @@ namespace MeshWelderAutocad.Commands.Laser
                 {
                     _panel = panel;
                     Document newDoc = acadApp.DocumentManager.Add(templatePath);
-                    Database db = newDoc.Database;
+                    _db = newDoc.Database;
 
                     var path = Path.Combine(generalDwgDirectory, $"{panel.Name}.dxf");
 
                     using (DocumentLock docLock = newDoc.LockDocument())
                     {
-                        CreateLayers(db, panel);
-                        using (Transaction tr = db.TransactionManager.StartTransaction())
+                        using (Transaction tr = _db.TransactionManager.StartTransaction())
                         {
                             _activeTransaction = tr;
-                            BlockTable blockTable = tr.GetObject(db.BlockTableId, OpenMode.ForRead) as BlockTable;
+                            BlockTable blockTable = tr.GetObject(_db.BlockTableId, OpenMode.ForRead) as BlockTable;
                             _modelSpace = tr.GetObject(blockTable[BlockTableRecord.ModelSpace], OpenMode.ForWrite) as BlockTableRecord;
-                            _layerTable = (LayerTable)tr.GetObject(db.LayerTableId, OpenMode.ForWrite);
-                            CreateFormwork();
-                            CreateOpenings();
+                            _layerTable = (LayerTable)tr.GetObject(_db.LayerTableId, OpenMode.ForWrite);
+                            CreateFormwork("1. Опалубка");
+                            CreateOpenings("1. Опалубка");
                             CreateConnections(_panel.Connection1, "3. Связи 1");
                             CreateConnections(_panel.Connection2, "4. Связи 2");
-                            //CreateEmbeddedDetail5();
-                            //CreateEmbeddedDetail6And9();
-                            //CreateEmbeddedDetail7();
-                            //CreatePockets();
-                            //CreateAnchors();
+                            CreateEmbeddedDetail5("9. ЗД 1.5");
+                            CreateEmbeddedDetail6And9();
+                            CreateEmbeddedDetail7("8. ЗД 1.7");
+                            CreatePockets("7. Карманы");
+                            CreateLoops("2. Петли");
+                            CreateAnchors("5. Анкера");
                             tr.Commit();
                         }
                         newDoc.Database.DxfOut(path, 12, DwgVersion.AC1024);
@@ -67,13 +70,26 @@ namespace MeshWelderAutocad.Commands.Laser
                 }
                 //File.Delete(jsonFilePath);
             }
-            catch(CustomException e)
+            catch (CustomException e)
             {
                 MessageBox.Show(e.Message, "Ошибка");
             }
-            catch(System.Exception e)
+            catch (System.Exception e)
             {
                 MessageBox.Show(e.Message + e.StackTrace, "Системная ошибка");
+            }
+        }
+
+        private static void CreateLoops(string layerName)
+        {
+            if (_panel.Loops.Count != 0)
+            {
+                CreateLayer(_db, layerName);
+                ObjectId layerId = _layerTable[layerName];
+                foreach (var loop in _panel.Loops)
+                {
+                    CreateLine(loop.X, _panel.Formwork.MaxYPanel - 600, loop.X, _panel.Formwork.MaxYPanel + 200, layerId);
+                }
             }
         }
 
@@ -81,6 +97,7 @@ namespace MeshWelderAutocad.Commands.Laser
         {
             if (connections.Count != 0)
             {
+                CreateLayer(_db, layerName);
                 ObjectId layerId = _layerTable[layerName];
                 foreach (var connection in connections)
                 {
@@ -104,34 +121,116 @@ namespace MeshWelderAutocad.Commands.Laser
             }
         }
 
-        private static void CreateAnchors()
+        private static void CreateAnchors(string layerName)
         {
-            throw new NotImplementedException();
+            if (_panel.Anchors.Count != 0)
+            {
+                CreateLayer(_db, layerName);
+                ObjectId layerId = _layerTable[layerName];
+                foreach (var anchor in _panel.Anchors)
+                {
+                    CreateLine(anchor.X, _panel.Formwork.MaxYPanel - 300, anchor.X, _panel.Formwork.MaxYPanel + 300, layerId);
+                }
+            }
         }
 
-        private static void CreatePockets()
+        private static void CreatePockets(string layerName)
         {
-            throw new NotImplementedException();
+            double width = 190.0;
+            double height = 170.0;
+            if (_panel.Pockets.Count != 0)
+            {
+                CreateLayer(_db, layerName);
+                ObjectId layerId = _layerTable[layerName];
+                foreach (var pocket in _panel.Pockets)
+                {
+                    double minX = pocket.X - width / 2.0;
+                    double maxX = pocket.X + width / 2.0;
+                    double minY = _panel.Formwork.MinYPanel;
+                    double maxY = _panel.Formwork.MinYPanel + height;
+
+                    CreateLine(minX, minY, minX, maxY, layerId);
+                    CreateLine(minX, maxY, maxX, maxY, layerId);
+                    CreateLine(maxX, maxY, maxX, minY, layerId);
+                    CreateLine(maxX, minY, minX, minY, layerId);
+                }
+            }
         }
 
-        private static void CreateEmbeddedDetail7()
+        private static void CreateEmbeddedDetail7(string layerName)
         {
-            throw new NotImplementedException();
+            double width = 210.0;
+            double height = 240.0;
+            if (_panel.EmbeddedParts7.Count != 0)
+            {
+                CreateLayer(_db, layerName);
+                ObjectId layerId = _layerTable[layerName];
+                foreach (var detail7 in _panel.EmbeddedParts7)
+                {
+                    double minX = detail7.X - width / 2.0;
+                    double maxX = detail7.X + width / 2.0;
+                    double minY = detail7.Y - height / 2.0;
+                    double maxY = detail7.Y + height / 2.0;
+
+                    CreateLine(minX, minY, minX, maxY, layerId);
+                    CreateLine(minX, maxY, maxX, maxY, layerId);
+                    CreateLine(maxX, maxY, maxX, minY, layerId);
+                    CreateLine(maxX, minY, minX, minY, layerId);
+                }
+            }
         }
 
         private static void CreateEmbeddedDetail6And9()
         {
-            throw new NotImplementedException();
+            double width = 220.0;
+            if (_panel.EmbeddedParts6.Count != 0)
+            {
+                CreateLayer(_db, "6. ЗД 1.6");
+                ObjectId layerId = _layerTable["6. ЗД 1.6"];
+                foreach (var detail6 in _panel.EmbeddedParts6)
+                {
+                    double maxY = _panel.Formwork.MaxYPanel - 240.0 + 30.0;
+                    double minY = _panel.Formwork.MaxYPanel - 240.0 - 30.0;
+                    double minX = detail6.X - width / 2.0;
+                    double maxX = detail6.X + width / 2.0;
+                    CreateLine(minX, minY, minX, maxY, layerId);
+                    CreateLine(maxX, minY, maxX, maxY, layerId);
+                }
+            }
+            else if (_panel.EmbeddedParts9.Count != 0)
+            {
+                CreateLayer(_db, "6. ЗД 1.9");
+                ObjectId layerId = _layerTable["6. ЗД 1.9"];
+                foreach (var detail6 in _panel.EmbeddedParts6)
+                {
+                    double maxY = _panel.Formwork.MaxYPanel - 240.0 + 30.0;
+                    double minY = _panel.Formwork.MaxYPanel - 240.0 - 30.0;
+                    double minX = detail6.X - width / 2.0;
+                    double maxX = detail6.X + width / 2.0;
+                    CreateLine(minX, minY, minX, maxY, layerId);
+                    CreateLine(maxX, minY, maxX, maxY, layerId);
+                }
+            }
         }
 
-        private static void CreateEmbeddedDetail5()
+        private static void CreateEmbeddedDetail5(string layerName)
         {
-            throw new NotImplementedException();
+            double width = 360.0;
+            double height = 240.0;
+            if (_panel.EmbeddedParts5.Count != 0)
+            {
+                CreateLayer(_db, layerName);
+                ObjectId layerId = _layerTable[layerName];
+                foreach (var detail5 in _panel.EmbeddedParts5)
+                {
+
+                }
+            }
         }
-        private static void CreateOpenings()
+        private static void CreateOpenings(string layerName)
         {
             //TODO у нижней линии дубляж с опалубкой может быть получается
-            ObjectId layerId = _layerTable["1. Опалубка"];
+            ObjectId layerId = _layerTable[layerName];
             foreach (var opening in _panel.Formwork.Openings)
             {
                 CreateLine(opening.MinX, opening.MinY,
@@ -145,9 +244,10 @@ namespace MeshWelderAutocad.Commands.Laser
             }
         }
 
-        private static void CreateFormwork()
+        private static void CreateFormwork(string layerName)
         {
-            ObjectId layerId = _layerTable["1. Опалубка"];
+            CreateLayer(_db, layerName);
+            ObjectId layerId = _layerTable[layerName];
             CreateLine(_panel.Formwork.MinXPanel, _panel.Formwork.MinYPanel,
                        _panel.Formwork.MinXPanel, _panel.Formwork.MaxYPanel, layerId);
             CreateLine(_panel.Formwork.MinXPanel, _panel.Formwork.MaxYPanel,
@@ -164,19 +264,6 @@ namespace MeshWelderAutocad.Commands.Laser
             line.LayerId = layerId;
             _modelSpace.AppendEntity(line);
             _activeTransaction.AddNewlyCreatedDBObject(line, true);
-        }
-
-        private static void CreateLayers(Database db, Dtos.Panel panel)
-        {
-            CreateLayer(db, "1. Опалубка");
-            CreateLayer(db, "2. Петли");
-            CreateLayer(db, "3. Связи 1");
-            if (panel.Connection2.Count != 0) CreateLayer(db, "4. Связи 2");
-            CreateLayer(db, "5. Анкера");
-            CreateLayer(db, "6. ЗД 1.6 (ЗД 1.9)");
-            CreateLayer(db, "7. Карманы");
-            CreateLayer(db, "8. ЗД 1.7");
-            CreateLayer(db, "9. ЗД 1.5");
         }
 
         public static void CreateLayer(Database db, string name)
