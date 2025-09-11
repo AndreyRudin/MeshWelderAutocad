113403cf6a7ecdc76075569406bbceb452bb888e
AndreyRudin <rudin1996@mail.ru>
2025-09-11 21:04:41 +0700

    дату обновил

--- MeshWelderAutocad/App.cs
+++ MeshWelderAutocad/App.cs
@@ -121,7 +121,7 @@ namespace MeshWelderAutocad
         }
         private static RibbonPanel AddInfoPanel()
         {
-            string versionDate = "v25.08.02";
+            string versionDate = "v25.09.11";
             var rps = new RibbonPanelSource();
             rps.Title = "    INFO    ";
             RibbonPanel rp = new RibbonPanel();
